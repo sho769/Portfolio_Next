16fad8c34edfda4634d1a46ac03e8c2aa2509041
sho769 <shourya.vraman@gmail.com>
2024-08-15 16:28:00 -0400

    movie

--- src/app/moviemania/page.js
+++ src/app/moviemania/page.js
@@ -5,8 +5,7 @@ import MovieCard from "../components/MovieCard.jsx";
 import "./MovieCard.css";
 import searchIcon from "../../../public/search.svg";
 
-const API_URL =
-  "http://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}";
+const API_URL = "http://www.omdbapi.com/?apikey=35573564";
 
 export default function Home() {
   const [movies, setMovies] = useState([]);

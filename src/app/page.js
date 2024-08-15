a3aa99dd31d1d8f11a04a3c5c49754c43602facd
sho769 <shourya.vraman@gmail.com>
2024-08-15 18:33:36 -0400

    Hidden achievment

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
-        <AchievementsSection />
+        {/* <AchievementsSection /> */}
         <AboutSection />
         <ProjectsSection />
         <BlogSection />
